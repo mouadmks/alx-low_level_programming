a8bfb0a92fd8c17147bbfad9b48277ab6a563be7
mouadmks <mouad.makhoukh@ensi.ma>
2023-06-20 19:18:58 -0700

    mandory tsks

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * main - print _putchar
- * Return: 0 if succesful
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
-int _putchar(char c)
 int main(void)
 {
 	_putchar('_');
@@ -16,5 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-return (0);
+
+	return (0);
 }
